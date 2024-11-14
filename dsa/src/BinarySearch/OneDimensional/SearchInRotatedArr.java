bd6cca76e42455f2b04c4b2be4f5f8b227a34e83
Jheel Shah <20cs082@charusat.edu.in>
2024-11-14 23:13:42 +0530

    search in rotate sorted array

---
+++ dsa/src/BinarySearch/OneDimensional/SearchInRotatedArr.java
@@ -0,0 +1,40 @@
+package BinarySearch.OneDimensional;
+
+import java.util.Scanner;
+
+public class SearchInRotatedArr {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[sc.nextInt()];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        search(arr, sc.nextInt());
+    }
+    private static int search(int[] nums,int target){
+        int start =0;
+        int end = nums.length-1;
+        int mid = (start+end)/2;
+
+        while (start<=end){
+            if(nums[mid] == target){
+                return mid;
+            }
+            if (nums[start] <= nums[mid]){
+                if(nums[start] <= target && target< nums[mid]){
+                    end = mid-1;
+                }
+                else start = mid+1;
+            }
+            else {
+                if(nums[mid] <target &&nums[end] >= target){
+                    mid = start+1;
+                }
+                else end = mid-1;
+            }
+            mid = (start+end)/2;
+        }
+        return -1;
+    }
+}
