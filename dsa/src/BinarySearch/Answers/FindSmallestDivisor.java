6cbe53aa97715ab0eda94a0f06cf4b85e8d44fbe
Jheel Shah <20cs082@charusat.edu.in>
2024-11-22 23:46:11 +0530

    find smallest divisior

---
+++ dsa/src/BinarySearch/Answers/FindSmallestDivisor.java
@@ -0,0 +1,46 @@
+package BinarySearch.Answers;
+
+import java.util.Scanner;
+
+public class FindSmallestDivisor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[sc.nextInt()];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        smallestDivisor(arr, sc.nextInt(), sc.nextInt());
+    }
+    private static int smallestDivisor(int[] nums,int threshold){
+        int n = nums.length;
+        int maxEl = nums[0];
+        for(int i = 1;i<n;i++){
+            if(nums[i] > maxEl){
+                maxEl = nums[i];
+            }
+        }
+        int start = 1;
+        int end = maxEl;
+        int mid = (start+end)/2;
+        int ans = -1;
+        while (start<=end){
+            if(calcDivResult(nums,n,mid) <=threshold){
+                ans = mid;
+                end = mid-1;
+            }
+            else start = mid+1;
+
+            mid = (start+end)/2;
+        }
+        return ans;
+    }
+    private static int calcDivResult(int[] nums,int n , int divisor){
+        int ans = 0;
+
+        for(int i = 0 ;i<n;i++){
+            ans += Math.ceilDiv(nums[i],divisor);
+        }
+        return ans;
+    }
+}
