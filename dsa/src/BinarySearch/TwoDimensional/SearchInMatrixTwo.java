bbcae9df347080c3a9c8cef6d75529d7b80625ed
Jheel Shah <20cs082@charusat.edu.in>
2024-11-28 23:16:51 +0530

    search matrix two

--- dsa/src/BinarySearch/TwoDimensional/SearchInMatrixTwo.java
+++ dsa/src/BinarySearch/TwoDimensional/SearchInMatrixTwo.java
@@ -2,7 +2,7 @@ package BinarySearch.TwoDimensional;
 
 import java.util.Scanner;
 
-public class SearchInTwoD {
+public class SearchInMatrixTwo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -16,21 +16,16 @@ public class SearchInTwoD {
         searchMatrix(matrix,sc.nextInt());
     }
     private static boolean searchMatrix(int[][] matrix, int target){
-        int rows = matrix.length;
-        int cols = matrix[0].length;
+        int row = 0;
+        int col = matrix[0].length-1;
 
-        int start = 0;
-        int end = (rows * cols)-1;
-        int mid = (start+end)/2;
-
-        while (start <= end){
-            int row = mid/cols;
-            int col = mid%cols;
-
+        while(row < matrix.length && col>=0){
             if(matrix[row][col] == target) return true;
-            else if(matrix[row][col] < target) start = mid+1;
-            else end = mid-1;
-            mid = (start+end)/2;
+
+            else if(matrix[row][col] < target){
+                row +=1;
+            }
+            else col -=1;
         }
         return false;
     }
