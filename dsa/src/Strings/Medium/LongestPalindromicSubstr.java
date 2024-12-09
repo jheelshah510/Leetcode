923d2d2694f5c6d7499005e5794bee058da387b7
Jheel Shah <20cs082@charusat.edu.in>
2024-12-10 00:24:53 +0530

    longestPalindromeSubstring

--- dsa/src/Strings/Medium/LongestPalindromicSubstr.java
+++ dsa/src/Strings/Medium/LongestPalindromicSubstr.java
@@ -13,6 +13,35 @@ public class LongestPalindromicSubstr {
     }
 
     public static String longestPalindrome(String s) {
+        String res = "";
+        int len = 0;
+        int n = s.length();
 
+        for (int i = 0; i < s.length(); i++) {
+            int l = i;
+            int r = i;
+
+            while (l >= 0 && r < n && s.charAt(l) == s.charAt(r)) {
+                if (r - l + 1 > len) {
+                    res = s.substring(l, r + 1);
+                    len = r - l + 1;
+                }
+                l -= 1;
+                r += 1;
+            }
+            l = i;
+            r = i + 1;
+
+            while (l >= 0 && r < n && s.charAt(l) == s.charAt(r)) {
+                if (r - l + 1 > len) {
+                    res = s.substring(l, r + 1);
+                    len = r - l + 1;
+                }
+                l -= 1;
+                r += 1;
+            }
+        }
+        return res;
     }
+
 }
