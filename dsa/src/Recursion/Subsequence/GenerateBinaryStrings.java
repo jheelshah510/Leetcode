bc319d1a179e0af8dae06d427f3c572df00664e5
Jheel Shah <20cs082@charusat.edu.in>
2025-01-14 23:05:08 +0530

    generate binary strings

---
+++ dsa/src/Recursion/Subsequence/GenerateBinaryStrings.java
@@ -0,0 +1,24 @@
+package Recursion.Subsequence;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class GenerateBinaryStrings {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int k = sc.nextInt();
+        genBinaries(k,new char[3],0);
+    }
+
+    private static void genBinaries(int k,char[] temp,int ind){
+        if(ind == k){
+            String a = new String(temp);
+            System.out.println(a);
+            return;
+        }
+        temp[ind] = '0';
+        genBinaries(k,temp,ind+1);
+        temp[ind] = '1';
+        genBinaries(k,temp,ind+1);
+    }
+}
